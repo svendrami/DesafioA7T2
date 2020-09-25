e57c22abb4f81402d33af10a7a93de864cb96492
svendrami <62160419+svendrami@users.noreply.github.com>
2020-09-25 11:23:57 -0300

    Add files via upload

---
+++ DesafioA7T2/DesafioA7T2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesafioA7T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
